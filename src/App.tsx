c67f210555163b2f2c4842c3dfe21552d4980e04
Davidoar15 <davidoar15@gmail.com>
2023-07-16 14:38:32 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className='App'>
+      <h1>Google Translate</h1>
+    </div>
+  )
+}
+
+export default App
